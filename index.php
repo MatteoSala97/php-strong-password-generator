060063be37ce14184f97f753d776af5b39257fcd
Matteo Sala <matteo111997@gmail.com>
2024-02-27 14:04:04 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<?
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" data-bs-theme="dark">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="refresh" content="5">
+    <title>Strong Password Generator</title>
+
+    <!-- Boostrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+
+    <!-- FontAwesome library import link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+
+    <!-- My CSS -->
+    <link rel="stylesheet" href="./style.css">
+
+</head>
+<body>
+    <header>
+        <div class="container mt-5 text-center">
+            <h1>Strong Password Generator</h1>
+            <h5>A stronger and more reliable password generator than what we've previously created</h5>
+        </div>
+    </header>
+
+    <main>
+        <div>
+
+        </div>
+    </main>
+</body>
+</html>
